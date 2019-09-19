64a4d70001e877158970eac9e83db922a921862f
jaehee <kjha2142@naver.com>
2019-09-19 15:42:53 +0900

    11377번 열혈강호3

---
+++ baekjoon_c++/11377.cpp
@@ -0,0 +1,64 @@
+/*
+	11377번 열혈강호 3
+*/
+
+#include <iostream>
+#include <vector>
+#define MAX 1001
+
+using namespace std;
+
+vector<int> v[MAX];
+int select[MAX];
+bool checked[MAX];
+
+bool dfs(int x) {
+	for (int i = 0; i < v[x].size(); i++)
+	{
+		int t = v[x][i];
+		if (checked[t])
+			continue;
+		checked[t] = true;
+		if (select[t] == 0 || dfs(select[t])) {
+			select[t] = x;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(void) {
+	int N, M , K;
+	cin >> N >> M >> K;
+
+	for (int i = 1; i <= N; i++)
+	{
+		int work;
+		cin >> work;
+		for (int j = 0; j < work; j++)
+		{
+			int a;
+			cin >> a;
+			v[i].push_back(a);
+		}
+	}
+
+	int count = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		fill(checked, checked + MAX, false);
+		if (dfs(i))
+			count++;
+	}
+
+	int two = 0;
+	for (int i = 1; i <= N && two < K; i++)
+	{
+		fill(checked, checked + MAX, false);
+		if (dfs(i))
+			two++;
+	}
+	cout << count + two << endl;
+	
+	return 0;
+}
