0d194fc8c110296ad3402231bf87e5f25b9c88bc
JaeHeee <kjha2142@naver.com>
2020-02-10 17:29:58 +0900

    2753번 윤년

---
+++ baekjoon_c++/2753.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+    
+    scanf("%d",&year);
+    
+    if(year%4==0 && (year%400==0||year%100!=0)){
+        printf("1");
+    } else{
+        printf("0");
+    }
+    
+    return 0;
+}
