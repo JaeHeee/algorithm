bf878fef4e1b2ecb288edc9b9ab16dbe56d4e034
JaeHeee <kjha2142@naver.com>
2020-02-10 17:24:13 +0900

    1330번 두 수 비교하기

---
+++ baekjoon_c++/1330.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1;
+    int num2;
+    
+    scanf("%d %d",&num1,&num2);
+    
+    if(num1 < num2){
+        printf("<");
+    } else if(num1 > num2) {
+        printf(">");
+    } else{
+        printf("==");
+    }
+    
+    return 0;
+}
