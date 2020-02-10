28a5d294ff85b7860773d20fb83630c6e0b17958
JaeHeee <kjha2142@naver.com>
2020-02-10 17:49:14 +0900

    10951번 A+B - 4

---
+++ baekjoon_c++/10951.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    int n,m;
+        
+    while(scanf("%d %d",&n,&m) != EOF) {
+        printf("%d\n",n+m);
+    }
+    
+    return 0;
+}
