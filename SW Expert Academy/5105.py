4a77d4d2c51f19ae41cdd5a9cebc1fe799eb5a58
JaeHeee <kjha2142@naver.com>
2020-04-27 17:21:13 +0900

    SW Expert Academy 5105번 - 미로의 거리

---
+++ SW Expert Academy/5105.py
@@ -0,0 +1,41 @@
+T = int(input())
+
+
+def bfs(s):
+    global result
+    if maze[s[0]][s[1]] == 3 or result == 1:
+        result = 1
+        return
+
+    visited.append([s[0], s[1]])
+    t = [0, 0]
+    for l in range(4):
+        t[0] = s[0] + dx[l]
+        t[1] = s[1] + dy[l]
+        if 0 <= t[0] < N and 0 <= t[1] < N and \
+                (maze[t[0]][t[1]] == 3 or maze[t[0]][t[1]] == 0) \
+                and [t[0], t[1]] not in visited:
+            distance[t[0]][t[1]] = distance[s[0]][s[1]] + 1
+            bfs(t)
+
+for i in range(T):
+    N = int(input())
+    maze = [list(map(int, input())) for j in range(N)]
+    visited = []
+    distance = [[0]*N for a in range(N)]
+    result = 0
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+
+    for k, m in enumerate(maze):
+        if 2 in m:
+            s = [k, m.index(2)]
+        if 3 in m:
+            e = [k, m.index(3)]
+
+    bfs(s)
+
+    if result == 0:
+        print("#%d %d" % (i + 1, result))
+    else:
+        print("#%d %d" % (i + 1, distance[e[0]][e[1]] - 1))
