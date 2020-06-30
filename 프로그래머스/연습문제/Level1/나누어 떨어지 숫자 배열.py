2a941c1609d221db6dd3e4912455b9ee670b6826
jaehee <kjha2142@naver.com>
2020-06-30 11:53:19 +0900

    level1

---
+++ 프로그래머스/연습문제/Level1/나누어 떨어지 숫자 배열.py
@@ -0,0 +1,11 @@
+def solution(arr, divisor):
+    answer = []
+    for num in arr:
+        if num % divisor == 0:
+            answer.append(num)
+
+    if answer:
+        answer.sort()
+        return answer
+    else:
+        return [-1]
