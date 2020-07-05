8256eaf011ce858b5d40f7260bb8cbecf6a56a83
jaehee <kjha2142@naver.com>
2020-07-05 09:53:18 +0900

    2020 카카오 인턴십

---
+++ Kakao 코딩 테스트/2020 카카오 인턴십/키패드 누르기.py
@@ -0,0 +1,46 @@
+def solution(numbers, hand):
+    answer = ''
+    left_side = [1, 4, 7, '*']
+    right_side = [3, 6, 9, '#']
+    center = [2, 5, 8, 0]
+    pos_l = '*'
+    pos_r = '#'
+    c_l = False
+    c_r = False
+    for n in numbers:
+        if n in left_side:
+            answer += 'L'
+            pos_l = n
+            c_l = False
+        elif n in right_side:
+            answer += 'R'
+            pos_r = n
+            c_r = False
+        else:
+            if c_l:
+                l_dist = abs(center.index(n) - center.index(pos_l))
+            else:
+                l_dist = abs(center.index(n) - left_side.index(pos_l)) + 1
+            if c_r:
+                r_dist = abs(center.index(n) - center.index(pos_r))
+            else:
+                r_dist = abs(center.index(n) - right_side.index(pos_r)) + 1
+            if l_dist < r_dist:
+                pos_l = n
+                c_l = True
+                answer += 'L'
+            elif l_dist > r_dist:
+                pos_r = n
+                c_r = True
+                answer += 'R'
+            else:
+                if hand == 'left':
+                    pos_l = n
+                    c_l = True
+                    answer += 'L'
+                else:
+                    pos_r = n
+                    c_r = True
+                    answer += 'R'
+
+    return answer
