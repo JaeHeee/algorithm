3ae0a1d5de87bb55daf640f22a8c5c4724e2dbc7
JaeHeee <kjha2142@naver.com>
2020-07-23 22:53:05 +0900

    오픈채팅방

---
+++ Kakao 코딩 테스트/2019 KAKAO BLIND RECRUITMENT/오픈채팅방.py
@@ -0,0 +1,20 @@
+def solution(record):
+    answer = []
+    records = dict()
+    result = []
+
+    for r in record:
+        temp = r.split(' ')
+
+        if temp[0] == 'Leave':
+            result.append([temp[1], "님이 나갔습니다."])
+        elif temp[0] == 'Enter':
+            records[temp[1]] = temp[2]
+            result.append([temp[1], "님이 들어왔습니다."])
+        else:
+            records[temp[1]] = temp[2]
+
+    for r in result:
+        answer.append(records[r[0]] + r[1])
+
+    return answer
