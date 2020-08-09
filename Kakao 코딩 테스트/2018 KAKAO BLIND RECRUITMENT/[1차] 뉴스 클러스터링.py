9573bfec579b3f3e5d64b3a2a6a2ba47526207d9
JaeHeee <kjha2142@naver.com>
2020-08-09 23:48:13 +0900

    [1차] 뉴스 클러스터링

---
+++ Kakao 코딩 테스트/2018 KAKAO BLIND RECRUITMENT/[1차] 뉴스 클러스터링.py
@@ -0,0 +1,30 @@
+def solution(str1, str2):
+    answer = 0
+    int_ = 0
+    uni_ = 0
+
+    multilist1 = [str1[i:i + 2].lower() for i in range(len(str1) - 1) if str1[i:i + 2].isalpha()]
+    multilist2 = [str2[i:i + 2].lower() for i in range(len(str2) - 1) if str2[i:i + 2].isalpha()]
+
+    multiset1 = set(multilist1)
+    multiset2 = set(multilist2)
+    multiint = multiset1.intersection(multiset2)
+    multiuni = multiset1.union(multiset2)
+
+    for i in multiint:
+        int1 = multilist1.count(i)
+        int2 = multilist2.count(i)
+        int_ += min(int1, int2)
+
+    for u in multiuni:
+        uni1 = multilist1.count(u)
+        uni2 = multilist2.count(u)
+        uni_ += max(un
+        i1, uni2)
+
+        if uni_ == 0:
+            answer = 1
+        else:
+            answer = int_ / uni_
+
+        return int(answer * 65536)
