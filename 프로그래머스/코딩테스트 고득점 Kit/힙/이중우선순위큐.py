30ac7b4f948dfcfc00a9eafa7d4e5a36e4209418
JaeHeee <kjha2142@gmail.com>
2020-08-26 15:39:50 +0900

    프로그래머스

--- 프로그래머스/코딩테스트 고득점 Kit/힙/이중우선순위큐.py
+++ 프로그래머스/코딩테스트 고득점 Kit/힙/이중우선순위큐.py
@@ -1,16 +1,26 @@
-def solution(operations):
-    answer = [0, 0]
-    queue = []
-    for op in operations:
-        a = op.split(' ')
-        if a[0] == 'I':
-            queue.append(int(a[1]))
-        else:
-            if a[1] == '1' and queue:
-                queue.remove(max(queue))
-            elif a[1] == '-1' and queue:
-                queue.remove(min(queue))
-    if queue:
-        answer = [max(queue), min(queue)]
+import heapq
 
-    return answer
+def solution(operations):
+    answer = [0,0]
+    heap = []
+
+    for op in operations:
+        op, num = op.split(" ")
+        # heap 구조를 만듭니다.
+        if op == 'I':
+            heapq.heappush(heap,int(num))
+        elif op =='D' and heap:
+            # D 다음의 숫자가 양수이면 max_heap구조로 변환하고, max 삭제
+            if int(num)>0:
+                heapq._heapify_max(heap)
+                heapq.heappop(heap)
+                if heap:
+                    heapq.heapify(heap)
+            # 음수이면 min 삭제
+            else:
+                heapq.heappop(heap)
+    
+    if heap:
+        return [max(heap), min(heap)]
+    else:
+        return answer
