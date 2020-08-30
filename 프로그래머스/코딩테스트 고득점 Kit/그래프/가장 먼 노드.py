db184eb31bd1a7e8b3204cb0192a7664145bfde4
JaeHeee <kjha2142@gmail.com>
2020-08-30 11:16:06 +0900

    가장 먼 노드

--- 프로그래머스/코딩테스트 고득점 Kit/그래프/가장 먼 노드.py
+++ 프로그래머스/코딩테스트 고득점 Kit/그래프/가장 먼 노드.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
 
+# bfs 이용해서 노드별 최단경로 저장
 def bfs(start, graph):
     queue = []
     queue.append(start)
     checked = [0] * len(graph)
-    path_length = [0] * len(graph)
+    path_length = [0] * len(graph) # 최단경로
     checked[0] = True
 
     while queue:
@@ -21,6 +22,7 @@ def bfs(start, graph):
 def solution(n, edge):
     graph = defaultdict(list)
 
+    # 그래프 만들
     for e in edge:
         graph[e[0]].append(e[1])
         graph[e[1]].append(e[0])
