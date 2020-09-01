94a4d198fece488b32227911dac1262484599cbb
JaeHeee <kjha2142@gmail.com>
2020-09-01 14:42:55 +0900

    모의고사

--- 프로그래머스/코딩테스트 고득점 Kit/완전탐색/모의고사.py
+++ 프로그래머스/코딩테스트 고득점 Kit/완전탐색/모의고사.py
@@ -1,36 +1,52 @@
+from collections import defaultdict
+
+
 def solution(answers):
     answer = []
-    one = [1,2,3,4,5]
-    two = [2,1,2,3,2,4,2,5]
-    three = [3,3,1,1,2,2,4,4,5,5]
-    count = [[0,1],[0,2],[0,3]]
-    a,b,c = 0, 0, 0
-    for i in range(len(answers)):
-        if answers[i] == one[a]:
-            count[0][0] += 1
-        if answers[i] == two[b]:
-            count[1][0] += 1
-        if answers[i] == three[c]:
-            count[2][0] += 1
-        if a + 1 == 5:
-            a = 0
-        else:
-            a+=1
-        if b + 1 == 8:
-            b = 0
-        else:
-            b+=1
-        if c + 1 == 10:
-            c = 0
-        else:
-            c+=1
+    one = [1, 2, 3, 4, 5]
+    two = [2, 1, 2, 3, 2, 4, 2, 5]
+    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    one_idx, two_idx, three_idx = 0, 0, 0
 
-    count.sort()
-    if count[0][0] == count[2][0]:
-        return [1,2,3]
-    elif count[2][0] == count [1][0]:
-        answer = [count[2][1],count[1][1]]
-        answer.sort()
-        return answer
-    else:
-        return [count[2][1]]
+    count = defaultdict(int)
+
+    for a in answers:
+        # Count 딕셔너리 맞춘 갯수
+        if a == one[one_idx]:
+            count[1] += 1
+        if a == two[two_idx]:
+            count[2] += 1
+        if a == three[three_idx]:
+            count[3] += 1
+
+        # index 초기화
+        if one_idx + 1 == len(one):
+            one_idx = 0
+        else:
+            one_idx += 1
+        if two_idx + 1 == len(two):
+            two_idx = 0
+        else:
+            two_idx += 1
+        if three_idx + 1 == len(three):
+            three_idx = 0
+        else:
+            three_idx += 1
+
+    count_list = list(count.items())
+    count_list.sort(key=lambda x: x[1], reverse=True)
+
+    # 가장 많이 맞춘 사람 찾기
+    for c in count_list:
+        if answer:
+            if c[1] == count[answer[-1]]:
+                answer.append(c[0])
+            else:
+                break
+        else:
+            answer.append(c[0])
+
+    return sorted(answer)
+
+
+## max_time = 4.01ms
