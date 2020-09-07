f309a465cce165e2e08bef91c71ceb7ddb45cee2
JaeHeee <kjha2142@gmail.com>
2020-09-08 00:11:14 +0900

    징검다리

--- 프로그래머스/코딩테스트 고득점 Kit/이분탐색/징검다리.py
+++ 프로그래머스/코딩테스트 고득점 Kit/이분탐색/징검다리.py
@@ -2,36 +2,29 @@ def solution(distance, rocks, n):
     answer = 0
     rocks.sort()
     rocks.append(distance)
-    # dist = [rocks[0]]
 
-    #     for i in range(1,len(rocks)):
-    #         dist.append(rocks[i]-rocks[i-1])
+    # 각 바위 사이의 거리
+    dist = [rocks[0]]
+    for i in range(1,len(rocks)):
+        dist.append(rocks[i]-rocks[i-1])
 
     min_ = 0
     max_ = distance
 
     while min_ <= max_:
         mid = (max_ + min_) // 2
-        prev = 0
         count = 0
         min_dist = 1e9
-        # temp = dist[:]
+        temp = dist[:]
 
-        for i in range(len(rocks)):
-            if rocks[i] - prev < mid:
+        for j in range(len(temp)):
+            if temp[j] < mid:
                 count += 1
+                # 마지막이 아닌 경우
+                if j != len(temp)-1:
+                    temp[j+1] += temp[j]
             else:
-                min_dist = min(min_dist, rocks[i] - prev)
-                prev = rocks[i]
-
-        #         for j in range(len(temp)):
-        #             if temp[j] < mid:
-        #                 count +=1
-        #                 print(temp[j],mid)
-        #             else:
-        #                 min_dist = min(min_dist, temp[j])
-
-        #         print(temp, min_dist, count)
+                min_dist = min(min_dist, temp[j])
 
         if count > n:
             max_ = mid - 1
