99cff2d4c0d9f83f6a3f644217d5241fe726f4f1
JaeHeee <kjha2142@gmail.com>
2020-09-23 23:52:38 +0900

    최고의 집합.py

--- 프로그래머스/연습문제/Level3/최고의 집합.py
+++ 프로그래머스/연습문제/Level3/최고의 집합.py
@@ -1,5 +1,15 @@
 def solution(n, s):
     answer = []
+
+    # 최고의 집합이 존재하지 않는 경우
     if n > s:
         return [-1]
+
+    # 몫으로 채워주기
+    answer = [s // n for _ in range(n)]
+
+    # 뒤에서부터 1씩 증가시켜주기
+    for i in range(1, s % n + 1):
+        answer[-i] += 1
+
     return answer
